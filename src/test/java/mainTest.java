85bbad0695dc20d543c0f9757f6674b0e58fc6b9
dean4208 <doly6@naver.com>
2025-06-09 10:04:10 +0900

    login

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,5 +1,45 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class mainTest {
+    @Test
+    void kiumloginpass() {
+        String id = "ABC";
+        String password = "BTS";
 
+        Login app = new Login(id,password);
+
+        assertEquals(actual, expected);
+    }
+
+    void kiumloginfail() {
+        String id = "ABC";
+        String password = "BTS";
+
+        Login app = new Login(id,password);
+
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    void nemologinpass() {
+        String id = "ABC";
+        String password = "BTS";
+
+        Login app = new Login(id,password);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void nemologinfail() {
+        String id = "ABC";
+        String password = "BTS";
+
+        Login app = new Login(id,password);
+
+        assertEquals(actual, expected);
+    }
 }
