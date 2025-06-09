6b29426ae00b075bfb1cd006b6ad432aa0b0c573
m00nstarr <moonstar11430@gmail.com>
2025-06-09 09:36:55 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.18.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
