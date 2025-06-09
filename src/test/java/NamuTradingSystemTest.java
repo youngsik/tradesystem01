206458bad6f47673aa868a7666fb8db54f50f847
sequence-and-query <lifeisvector@gmail.com>
2025-06-09 12:15:32 +0900

    [REFACTORING] namu sell

--- src/test/java/NamuTradingSystemTest.java
+++ src/test/java/NamuTradingSystemTest.java
@@ -3,15 +3,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NamuTradingSystemTest {
+
+    public static final String STOCK_CODE = "T02";
+    public static final int STOCK_COUNT = 1;
+    public static final int STOCK_SELLING_PRICE = 2000;
+
     @Test
     public void sell() {
-        String stockCode = "T02";
-        int count = 1;
-        int price = 2000;
 
         NamuTradingSystem app = new NamuTradingSystem();
-        String actual = app.sell(stockCode,count,price);
-        String expected = String.format("%s를 %d 가격에 매도하였음", stockCode,price);
+        String actual = app.sell(STOCK_CODE, STOCK_COUNT, STOCK_SELLING_PRICE);
+        String expected = String.format("%s를 %d 가격에 매도하였음", STOCK_CODE, STOCK_SELLING_PRICE);
         assertEquals(actual, expected);
     }
 }
