1e79481448a6d37c4b3690b96433bb9247f0d7a1
hihi <you@example.com>
2025-06-09 15:44:30 +0900

    [refactoring] TradeManagerTest 리팩토링

--- src/test/java/TradeManagerTest.java
+++ src/test/java/TradeManagerTest.java
@@ -1,7 +1,7 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,74 +12,64 @@ class TradeManagerTest {
 
     public static final String ID = "ABC";
     public static final String CORRECT_PW = "BTS";
-    public static final String LOGIN_SUCCESS_LOG = "님 로그인 성공";
 
     public static final String STOCK_CODE = "T01";
     public static final int STOCK_COUNT = 1;
     public static final int STOCK_PRICE = 1000;
 
+    public static final String LOGIN_SUCCESS_RESPONSE = String.format("%s" + "님 로그인 성공", ID);
+    public static final String BUY_SUCCESS_RESPONSE = String.format("%s를 %d 가격에 매수하였음", STOCK_CODE, STOCK_PRICE);
+    public static final String SELL_SUCCESS_RESPONSE = String.format("%s를 %d 가격에 매도하였음", STOCK_CODE, STOCK_PRICE);
+
     @Mock
-    private TradingSystem tradingSystem ;
-    private final TradeManager trademanager= TradeManager.getInstance();
+    private TradingSystem tradingSystem;
+    private final TradeManager trademanager = TradeManager.getInstance();
+
+    @BeforeEach
+    void setUp() {
+        trademanager.selectStockBrocker(tradingSystem);
+    }
 
     @Test
     void TradeManagerLoginPass() {
-        trademanager.selectStockBrocker(tradingSystem);
-        String expected = String.format("%s" + LOGIN_SUCCESS_LOG, ID);
-        doReturn(ID + LOGIN_SUCCESS_LOG).when(tradingSystem).login(ID, CORRECT_PW);
-
+        doReturn(LOGIN_SUCCESS_RESPONSE).when(tradingSystem).login(ID, CORRECT_PW);
         String actual = trademanager.login(ID, CORRECT_PW);
-        assertEquals(expected, actual);
+        assertEquals(LOGIN_SUCCESS_RESPONSE, actual);
     }
 
     @Test
     void TradeManagerLoginPass2() {
-        trademanager.selectStockBrocker(tradingSystem);
-        String expected = String.format("%s" + LOGIN_SUCCESS_LOG, ID);
-        doReturn(ID + LOGIN_SUCCESS_LOG).when(tradingSystem).login(ID, CORRECT_PW);
-
+        doReturn(LOGIN_SUCCESS_RESPONSE).when(tradingSystem).login(ID, CORRECT_PW);
         String actual = trademanager.login(ID, CORRECT_PW);
-        assertEquals(expected, actual);
+        assertEquals(LOGIN_SUCCESS_RESPONSE, actual);
     }
 
     @Test
     void TradeManagerBuy1() {
-        trademanager.selectStockBrocker(tradingSystem);
-        String expected = String.format("%s를 %d 가격에 매수하였음", STOCK_CODE, STOCK_PRICE);
-        doReturn(expected).when(tradingSystem).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-
+        doReturn(BUY_SUCCESS_RESPONSE).when(tradingSystem).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
         String actual = trademanager.buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(expected, actual);
+        assertEquals(BUY_SUCCESS_RESPONSE, actual);
     }
 
     @Test
     void TradeManagerBuy2() {
-        trademanager.selectStockBrocker(tradingSystem);
-        String expected = String.format("%s를 %d 가격에 매수하였음", STOCK_CODE, STOCK_PRICE);
-        doReturn(expected).when(tradingSystem).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-
+        doReturn(BUY_SUCCESS_RESPONSE).when(tradingSystem).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
         String actual = trademanager.buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(expected, actual);
+        assertEquals(BUY_SUCCESS_RESPONSE, actual);
     }
 
     @Test
     void TradeManagerSell1() {
-        trademanager.selectStockBrocker(tradingSystem);
-        String expected = String.format("%s를 %d 가격에 매도하였음", STOCK_CODE, STOCK_PRICE);
-        doReturn(expected).when(tradingSystem).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-
+        doReturn(SELL_SUCCESS_RESPONSE).when(tradingSystem).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
         String actual = trademanager.sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(expected, actual);
+        assertEquals(SELL_SUCCESS_RESPONSE, actual);
     }
 
     @Test
     void TradeManagerSell2() {
-        trademanager.selectStockBrocker(tradingSystem);
-        String expected = String.format("%s를 %d 가격에 매도하였음", STOCK_CODE, STOCK_PRICE);
-        doReturn(expected).when(tradingSystem).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-
+        doReturn(SELL_SUCCESS_RESPONSE).when(tradingSystem).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
         String actual = trademanager.sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(expected, actual);
+        assertEquals(SELL_SUCCESS_RESPONSE, actual);
     }
 
 }
