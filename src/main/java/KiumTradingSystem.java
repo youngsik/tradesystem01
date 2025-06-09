30585d188887f6fa74432ac2005726230a09085d
hihi <you@example.com>
2025-06-09 11:25:45 +0900

    [refactoring] kiwerapi 추가 및 mock 테스트 추가

--- src/main/java/KiumTradingSystem.java
+++ src/main/java/KiumTradingSystem.java
@@ -1,6 +1,14 @@
 public class KiumTradingSystem {
 
+    private KiwerAPI kiwerAPI;
+
+    public KiumTradingSystem(KiwerAPI kiwerAPI) {
+        this.kiwerAPI = kiwerAPI;
+    }
+
     public String buy(String stockCode, int count, int price) {
+        kiwerAPI.buy(stockCode, count, price);
         return String.format("%s를 %d 가격에 매수하였음", stockCode, price);
     }
+
 }
--- src/test/java/KiumTradingSystemTest.java
+++ src/test/java/KiumTradingSystemTest.java
@@ -1,23 +1,33 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class KiumTradingSystemTest {
 
     private static final String STOCK_CODE = "T01";
     private static final int STOCK_COUNT = 1;
     private static final int STOCK_PRICE = 1000;
 
+    @Mock
+    private KiwerAPI mockKiwerAPI;
+
+    @InjectMocks
     private KiumTradingSystem kiumTradingSystem;
 
-    @BeforeEach
-    void setUp() {
-        kiumTradingSystem = new KiumTradingSystem();
+    @Test
+    void kiumbuytest_verifyapicall() {
+        kiumTradingSystem.buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
+        verify(mockKiwerAPI).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
     }
 
     @Test
-    void kiumbuypass() {
+    void kiumbuytest_pass() {
         String actual = kiumTradingSystem.buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
         String expected = String.format("%s를 %d 가격에 매수하였음", STOCK_CODE, STOCK_PRICE);
 
