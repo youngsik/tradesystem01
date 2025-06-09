d976a8fc2f31172813f03afbaffeeb10b1f61723
hihi <you@example.com>
2025-06-09 15:35:55 +0900

    [feature] TradeManagerTest 내 API Mock 제거 및 TradingSystem Mock ê°추가

--- src/test/java/TradeManagerTest.java
+++ src/test/java/TradeManagerTest.java
@@ -5,15 +5,10 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TradeManagerTest {
-
-    @Mock
-    private NemoApi nemoApi;
-
-    @Mock
-    private KiwerAPI kiwerAPI;
 
     public static final String ID = "ABC";
     public static final String CORRECT_PW = "BTS";
@@ -23,72 +18,68 @@ class TradeManagerTest {
     public static final int STOCK_COUNT = 1;
     public static final int STOCK_PRICE = 1000;
 
+    @Mock
     private TradingSystem tradingSystem ;
     private final TradeManager trademanager= TradeManager.getInstance();
 
     @Test
     void TradeManagerLoginPass() {
-        tradingSystem = new KiumTradingSystem();
         trademanager.selectStockBrocker(tradingSystem);
         String expected = String.format("%s" + LOGIN_SUCCESS_LOG, ID);
+        doReturn(ID + LOGIN_SUCCESS_LOG).when(tradingSystem).login(ID, CORRECT_PW);
 
         String actual = trademanager.login(ID, CORRECT_PW);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void TradeManagerLoginPass2() {
-        tradingSystem = new NemoTradingSystem(nemoApi);
         trademanager.selectStockBrocker(tradingSystem);
         String expected = String.format("%s" + LOGIN_SUCCESS_LOG, ID);
-        Mockito.doNothing().when(nemoApi).certification(ID, CORRECT_PW);
+        doReturn(ID + LOGIN_SUCCESS_LOG).when(tradingSystem).login(ID, CORRECT_PW);
 
         String actual = trademanager.login(ID, CORRECT_PW);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void TradeManagerBuy1() {
-        tradingSystem = new KiumTradingSystem(kiwerAPI);
         trademanager.selectStockBrocker(tradingSystem);
         String expected = String.format("%s를 %d 가격에 매수하였음", STOCK_CODE, STOCK_PRICE);
-        Mockito.doNothing().when(kiwerAPI).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
+        doReturn(expected).when(tradingSystem).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
 
         String actual = trademanager.buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void TradeManagerBuy2() {
-        tradingSystem = new NemoTradingSystem(nemoApi);
         trademanager.selectStockBrocker(tradingSystem);
         String expected = String.format("%s를 %d 가격에 매수하였음", STOCK_CODE, STOCK_PRICE);
-        Mockito.doNothing().when(nemoApi).purchasingStock(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
+        doReturn(expected).when(tradingSystem).buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
 
         String actual = trademanager.buy(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void TradeManagerSell1() {
-        tradingSystem = new KiumTradingSystem(kiwerAPI);
         trademanager.selectStockBrocker(tradingSystem);
         String expected = String.format("%s를 %d 가격에 매도하였음", STOCK_CODE, STOCK_PRICE);
-        Mockito.doNothing().when(kiwerAPI).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
+        doReturn(expected).when(tradingSystem).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
 
         String actual = trademanager.sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void TradeManagerSell2() {
-        tradingSystem = new NemoTradingSystem(nemoApi);
         trademanager.selectStockBrocker(tradingSystem);
         String expected = String.format("%s를 %d 가격에 매도하였음", STOCK_CODE, STOCK_PRICE);
-        Mockito.doNothing().when(nemoApi).sellingStock(STOCK_CODE, STOCK_PRICE, STOCK_COUNT);
+        doReturn(expected).when(tradingSystem).sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
 
         String actual = trademanager.sell(STOCK_CODE, STOCK_COUNT, STOCK_PRICE);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
